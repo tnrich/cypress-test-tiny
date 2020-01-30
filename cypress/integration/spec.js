9fc33e040b087431864fe5a9dd4525ddae2565a0
Thomas <trich1230@gmail.com>
2020-01-30 09:14:42 -0800

    setting up test case showing how onAnyRequest is not setting headers for subsequent cy.request calls

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,20 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+
+beforeEach(() => {
+  cy.server({
+    onAnyRequest: (route, proxy) => {
+      console.log("hit") //it doesn't look like this even gets hit..
+      proxy.xhr.setRequestHeader("my-token", "lalalalal");
+    }
+  });
 })
+describe("page", () => {
+  it("cy.server({onAnyRequest}) does not set headers on ", () => {
+    cy.wait(1).then(() => {
+      console.log("hit2")
+      cy.request("https://example.cypress.io").then(() => {
+        console.log("hit3")
+      })
+    })
+  });
+});
