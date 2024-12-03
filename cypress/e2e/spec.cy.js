5d9c0afcec0c647a98ecf3a8701943303dd5334c
Thomas Willheim <tnrich@teselagen.com>
2024-12-03 09:53:15 -0800

    test: update spec to throw error with long string

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit("https://example.cypress.io");
+    const longString = "a".repeat(1000000);
+    throw new Error(longString);
   })
 })
